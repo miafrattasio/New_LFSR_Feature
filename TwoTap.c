9b5dbe1a8cd84a45430e8d04d41450bf7602478b
Ryan Vernick <rpvernick@wpi.edu>
2026-03-29 23:02:59 -0400

    increased understandability of the file and process
    - formatted comments for readability
    - removed the LFSR_LENGTH constant
    (as well as dealing with the dependencies)
    - clarified what the secret key would include and included default values for them (should be made random at some point)

--- TwoTap.c
+++ TwoTap.c
@@ -19,49 +19,68 @@
 #define LFSR2_GATE_A  0b101101101  // 1 + x^1 + x^3 + x^4 + x^6 + x^7 + x^9
 #define LFSR2_GATE_B  0b000100001  // 1 + x^4 x^9
 
-#define LFSR_LENGTH 9 
+//#define LFSR_LENGTH 9 
 
 
-/*
-void clockCycle(int *stateA_ptr, int *stateB_ptr)
-
-simulates a clock cycle on LFSR_A and the subsequent actions of LFSR_B, and prints the outpt of the clock cycle
-
-inputs:
-  int stateA_ptr: a pointer to the state of LFSR_A
-  int stateB_ptr: a pointer to the state of LFSR_B
-
-modifies:
-  the integers pointed to by the inputs
-
+/**
+ * void clockCycle(int *stateA_ptr, int *stateB_ptr)
+ * 
+ * simulates a clock cycle on LFSR_A and the subsequent actions of LFSR_B, and prints the outpt of the clock cycle
+ * 
+ * inputs:
+ *   int stateA_ptr: a pointer to the state of LFSR_A
+ *   int stateB_ptr: a pointer to the state of LFSR_B
+ * 
+ * modifies:
+ *   the integers pointed to by the inputs
 */
 void clockCycle(int *lfsr1_state_ptr, int *lfsr2_state_ptr);
 
-/*
-int getFeedback(int state, int taps)
-
-  takes a state and set of gates (taps) and returns the output bit of the LFSR
-  State and taps must both be of thength N, where N = LFSR_LENGTH
-
-  input:
-    state of the N-bit LFSR
-    gates for that LFSR
-
-  output:
-    1 or 0, depending on what the output bit is
+/**
+ * int advance(int *state_ptr, int taps, int length)
+ * 
+ * advances a given LFSR
+ * 
+ * input:
+ *  int *state: a pointer to a LFSR state
+ *  int taps:   the gate positions of the passed LFSR
+ *  int length  how many bits are in the state/gate of the LFSR
+ * 
+ * modifies:
+ *  the state that state_ptr points to
+ *
+ * output:
+ *  the output of the LFSR given the state and gates
 */
-int getFeedback(int state, int taps);
+int advance(int *state_ptr, int taps, int length);
 
-/*
-void printBinary(int n, int bits)
+/**
+ * int getFeedback(int state, int taps, int length)
+ * 
+ * takes a state and set of gates (taps) and returns the output bit of the LFSR
+ * determines new bit that will be fed into the shift register
+ * 
+ * State and taps must both be of thength N, where N = length
+ * 
+ * input:
+ *   current state of the N-bit LFSR
+ *   gates for that LFSR
+ *   how many bits are in the state/gate of the lfsr
+ * 
+ * output:
+ *   1 or 0, depending on what the output bit is
+*/
+int getFeedback(int state, int taps, int length);
 
-prints the first bits of the binary representation of an integer,
-for printing LFSRs, 'bits' is the length of the LFSR (# of registers)
-
-inputs:
-  n : an integer
-  bits: in integer
-
+/**
+ * void printBinary(int n, int bits)
+ * 
+ * prints the first bits of the binary representation of an integer,
+ * for printing LFSRs, 'bits' is the length of the LFSR (# of registers)
+ * 
+ * inputs:
+ *   n : an integer
+ *   bits: in integer
 */
 void printBinary(int n, int bits);
 
@@ -70,14 +89,16 @@ void printBinary(int n, int bits);
 
 
 int main() {
-
-
-
-  //Initial seeds/secret keys
-  //if we start with all 0's, feedback with always be 0, register will be stuck
-  //with two registers, total key is 18 bits
+  // Initial seeds/secret keys
+  // if we start with all 0's, feedback with always be 0, register will be stuck
+  // Total key is 36 "Truly Random" Bits.
   int lfsr1_state = 0b110101010; 
   int lfsr2_state = 0b101110001;
+  int lsfr3_state = 0b010011001;
+  int lsfr3_gate  = 0b110001101;
+
+
+  int lsfr3_gate2;
   
   printf("Cycle | LFSR1 Bit | Tap Used | LFSR2 State | Output Bit\n");
   printf("-------------------------------------------------------\n");
@@ -92,6 +113,7 @@ int main() {
     clockCycle(&lfsr1_state, &lfsr2_state);
 
   }
+
     return 0;
 }
 
@@ -108,7 +130,7 @@ void clockCycle(int *lfsr1_state_ptr, int *lfsr2_state_ptr){
 
 
   //controller/LFSR1
-  int lfsr1Bit = getFeedback(lfsr1_state, LFSR_1_GATE);
+  int lfsr1Bit = getFeedback(lfsr1_state, LFSR_1_GATE, 9);
   
   // Choose taps for lsfr2 based on the controller lfsr(lfsr1)
   //this is where the modification happens, rather than using one set of taps, use if else to swap the rules
@@ -119,7 +141,7 @@ void clockCycle(int *lfsr1_state_ptr, int *lfsr2_state_ptr){
   char* tapName = (lfsr1Bit == 1) ? "A" : "B";
   
   //lfsr using selected taps
-  int lfsr2Bit = getFeedback(lfsr2_state, currentTaps);
+  int lfsr2Bit = getFeedback(lfsr2_state, currentTaps, 31);
   
   //ouput the values before shifting
   printf("%5d |    %d     |  %s   |  ", cycles, lfsr1Bit, tapName);
@@ -128,30 +150,39 @@ void clockCycle(int *lfsr1_state_ptr, int *lfsr2_state_ptr){
   printf("  |    %d\n", lfsr2Bit);
   
   //shift and update the states
-  // stateA >> 1 means to shift all bits one position to the right (Which drops oldest bit?)
-  // lfsr1Bit << 8 means to take the new feedback bit and move to 9th position (far most left pos)
+  // stateA >> 1 means to shift all bits one position to the right (Which drops rightmost bit)
+  // lfsr1Bit << 8 means to take the new feedback bit and move to 9th position (far most left pos, in a 9-bit LFSR)
   // the OR combines the shifted state with the new bit
   // & 0x1FF- that is binary 111111111, this is here to ensure that if any bits shifted past the 9th, they are dropped, keeping 9 bits
-  *lfsr1_state_ptr = ((lfsr1_state >> 1) | (lfsr1Bit << (LFSR_LENGTH-1))) & 0b111111111;
-  *lfsr2_state_ptr = ((lfsr2_state >> 1) | (lfsr2Bit << (LFSR_LENGTH-1))) & 0b111111111;
+  *lfsr1_state_ptr = ((lfsr1_state >> 1) | (lfsr1Bit << (9-1))) & 0b111111111;  //lfsr1 is 9 bits long
+  *lfsr2_state_ptr = ((lfsr2_state >> 1) | (lfsr2Bit << (31-1))) & 0b1111111111111111111111111111111; //lfsr2 is 31 bits long
 
   cycles ++;
 
 }
 
-// helper function to get the feedback bit
-// determines new bit that will be fed into the shift register
-// inputs take current state (bits currently in register) and the taps (positions that are wired together to created the feedback)
-// the XOR is the binary math, XORing a series of bits is like checking if the sum of 1s is even (0) or odd(1), and use % 2
-int getFeedback(int state, int taps) {
+int advance(int *state_ptr, int taps, int length){
+
+  int state = *state_ptr;
+
+  int outputBit = getFeedback(state, taps, length);
+
+  *state_ptr = ((state >> 1) | (outputBit << (length-1)));
+
+  return outputBit;
+
+}
+
+int getFeedback(int state, int taps, int length) {
   int xorSum = 0;
-  for (int i = 0; i < LFSR_LENGTH; i++) {
+  for (int i = 0; i < length; i++) {
 
     // _&1 == _%2
     if ((taps >> i) & 1 && (state >> i) & 1) {
       xorSum++;
     }
   }
+  // the XOR is the binary math, XORing a series of bits is like checking if the sum of 1s is even (0) or odd(1), and use % 2
   return xorSum % 2; 
 }
 
